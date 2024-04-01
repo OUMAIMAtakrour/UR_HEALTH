6fc79bd655ff20067209aea9439a9587ddf57896
OUMAIMAtakrour <takrouroumaima@gmail.com>
2024-04-01 10:43:35 +0000

    update

---
+++ e-health/src/App.jsx
@@ -0,0 +1,19 @@
+
+import { BeakerIcon } from '@heroicons/react/24/solid'
+import './App.css'
+
+function App() {
+  
+
+  return (
+   
+     <div className='App bg-purple-500 '>
+      App.jsx
+      <BeakerIcon className='w-4'/>
+    
+     </div>
+   
+  )
+}
+
+export default App
