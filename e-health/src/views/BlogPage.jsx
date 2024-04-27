e2ecaf28e6e89d4d6ef732defa099e8320be70ca
OUMAIMAtakrour <takrouroumaima@gmail.com>
2024-04-27 18:46:47 +0100

    routes conflicts

--- e-health/src/views/BlogPage.jsx
+++ e-health/src/views/BlogPage.jsx
@@ -3,44 +3,11 @@ import axiosClient from "../helpers/axios";
 import AddPostForm from "../Components/AddBlog";
 import { useStateContext } from "../contexts/ContextProvider";
 import { Navigate } from "react-router-dom";
-const BlogCard = ({ imageSrc, title, content }) => {
-    const { currentUser, userToken } = useStateContext();
-    if (!userToken) {
-        return <Navigate to="/signup" />;
-    }
-    return (
-        <div className="flex flex-col bg-white rounded-lg shadow-lg w-64 h-auto">
-            <img
-                src={imageSrc}
-                alt="Blog post image"
-                className="w-full h-48 object-cover object-center rounded-t-lg  bg-cover bg-center"
-            />
-            <div className="px-8 py-4">
-                <h2 className="text-xl font-bold">{title}</h2>
-                <p className="text-gray-700">Content: {content}</p>{" "}
-            </div>
-        </div>
-    );
-};
+import BlogCard from "../Components/BlogCard";
 
 const BlogPage = () => {
     const [blogs, setBlogs] = useState([]);
     const [hovered, setHovered] = useState(false);
-
-    useEffect(() => {
-        const fetchBlogs = async () => {
-            try {
-                const response = await axiosClient.get("/blogs");
-                const responseData = response.data;
-                const blogsData = responseData.data;
-                setBlogs(blogsData);
-            } catch (error) {
-                console.error("Error fetching blogs:", error);
-            }
-        };
-
-        fetchBlogs();
-    }, []);
 
     return (
         <div className="flex flex-col min-h-screen">
--- e-health/src/views/Signup.jsx
+++ e-health/src/views/Signup.jsx
@@ -3,7 +3,6 @@ import axiosClient from "../helpers/axios";
 import { data } from "autoprefixer";
 import { useNavigate } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
-import axios from "axios";
 
 function Signup() {
     const [user, setUser] = useState({
@@ -42,8 +41,20 @@ function Signup() {
                 console.log(data);
                 setCurrentUser(data.user);
                 localStorage.setItem("userToken", data.token);
-
-                navigate("/blog");
+                switch (data.user.role) {
+                    case "patient":
+                        navigate("/blog");
+                        break;
+                    case "doctor":
+                        navigate("/doc");
+                        break;
+                    case "admin":
+                        navigate("/dashboard");
+                        break;
+                    default:
+                        navigate("/"); // Default fallback route
+                        break;
+                }
             })
             .catch(({ error }) => {
                 if (error.response) {
@@ -77,12 +88,6 @@ function Signup() {
                 <div className="absolute -top-20 -right-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8" />
             </div>
             <div className="flex md:w-1/2 justify-center py-10 items-center bg-white">
-                {error.__html && (
-                    <div
-                        className="bg-blue-500 rounded py-2 px-3 w-44 text-white"
-                        dangerouslySetInnerHTML={error}
-                    ></div>
-                )}
                 <form onSubmit={onSubmit} className="bg-white" method="POST">
                     <h1 className="text-gray-800 font-bold text-2xl mb-1">
                         Hello Again!
@@ -155,7 +160,7 @@ function Signup() {
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
                                 strokeWidth={2}
-                                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                                d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
                             />
                         </svg>
                         <select
@@ -198,6 +203,12 @@ function Signup() {
                     >
                         SignUp
                     </button>
+                    {error.__html && (
+                        <div
+                            className="bg-blue-500 rounded py-2 px-3 w-44 text-white"
+                            dangerouslySetInnerHTML={error}
+                        ></div>
+                    )}
                     <span className="text-sm ml-2 hover:text-blue-500 cursor-pointer">
                         Forgot Password ?
                     </span>
